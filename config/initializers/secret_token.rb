cb271d5958764d9d2727987561c2eeecad83c93b
Kenechukwu Onodu <kayceeonodu@yahoo.com>
2013-05-03 12:54:30 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CreditcardChecker::Application.config.secret_token = '27f1d7de9a468ab282761f8c94641333207c60ddf721e47e9c0932649b1e987cc392bb56c5e67e37c14be72ec33ccd2a3f78f40d3694027c3212f013e3cb7ff7'
